1de943a3ed7d07bc06e93f252b227bfb6e77b111
presbrey <presbrey@users.noreply.github.com>
2014-08-16 23:50:13 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+var (
+	bind = flag.String("bind", ":8080", "HTTP listener address")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+	host, _, _ := net.SplitHostPort(req.RemoteAddr)
+	xff := req.Header.Get("X-Forwarded-For")
+	rw.Header().Set("Content-Type", "text/plain")
+	if len(xff) > 0 {
+		fmt.Fprintf(rw, "%s, %s\n", xff, host)
+	} else {
+		fmt.Fprintf(rw, "%s\n", host)
+	}
+}
+
+func main() {
+	http.ListenAndServe(*bind, http.HandlerFunc(handler))
+}
